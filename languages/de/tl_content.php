3f1e549752334316fb67933ab56a3fa57ffa087a
Gerald Meier <garyee@gmx.de>
2015-02-03 12:23:13 +0100

    label update

--- languages/de/tl_content.php
+++ languages/de/tl_content.php
@@ -1,10 +1,12 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_content']['venoBoxColumn0'] = 'Typ';
-$GLOBALS['TL_LANG']['tl_content']['venoBoxColumn1'] = 'In Gallerie';
-$GLOBALS['TL_LANG']['tl_content']['venoBoxColumn2'] = 'Link-Url';
-$GLOBALS['TL_LANG']['tl_content']['venoBoxColumn3'] = 'Link-Beschreibung';
-$GLOBALS['TL_LANG']['tl_content']['venoBoxColumn4'] = 'Link-Text';
+$GLOBALS['TL_LANG']['tl_content']['venoBoxColumn0'][] = 'Typ';
+$GLOBALS['TL_LANG']['tl_content']['venoBoxColumn1'][] = 'In Gallerie';
+$GLOBALS['TL_LANG']['tl_content']['venoBoxColumn2'][] = 'Link-Url';
+$GLOBALS['TL_LANG']['tl_content']['venoBoxColumn3'][] = 'Link-Beschreibung';
+$GLOBALS['TL_LANG']['tl_content']['venoBoxColumn3'][] = 'Wird in der Lightbox oben angezeigt';
+$GLOBALS['TL_LANG']['tl_content']['venoBoxColumn4'][] = 'Link-Text';
+$GLOBALS['TL_LANG']['tl_content']['venoBoxColumn4'][] = 'Der Text der verlinkt wird';
 $GLOBALS['TL_LANG']['tl_content']['listitems'][0]="Venobox Gallerie-Objekte";
 $GLOBALS['TL_LANG']['tl_content']['listitems'][1]="Wählen sie den Typen und geben sie die Details ein";
 $GLOBALS['TL_LANG']['tl_content']['venobox_list_legend']="VenoBox Liste";
