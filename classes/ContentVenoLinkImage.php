d69318c1844903d5c1665d4ad525cda2f3778d01
theDyingMountain <mario_gates@ymail.com>
2017-07-07 08:30:00 +0200

    move strTemplate switch to generate

--- classes/ContentVenoLinkImage.php
+++ classes/ContentVenoLinkImage.php
@@ -13,20 +13,24 @@
 
 namespace postyou;
 
-
 use Contao\ContentImage;
 use Contao\Input;
 
 class ContentVenoLinkImage extends ContentImage
 {
-
     protected $strTemplate = 'ce_veno_image_3.4';
+
+    public function generate()
+    {
+        if (version_compare(VERSION, '3.5', '>=')) {
+            $this->strTemplate='ce_veno_image_3.5';
+        }
+        return parent::generate();
+    }
+
 
     protected function compile()
     {
-        if (version_compare(VERSION, '3.50', '>=')) {
-            $this->strTemplate='ce_veno_image_3.5';
-        }
         parent::compile();
         if ($this->fullsize==2) {
             $venobox=new VenoElement($this->venoList);
@@ -34,5 +38,4 @@ class ContentVenoLinkImage extends ContentImage
             $venobox->setTemplateVars4ImageTempl($this->Template);
         }
     }
-
 }
